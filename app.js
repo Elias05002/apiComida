0536662a0805af851763983e7e9c3bb5479c23ae
alondra <alondradavila14002@gmail.com>
2024-06-13 17:56:37 -0600

    segundo edit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express")
+const morgan = require("morgan");
 const app = express()
 const port = 4000
+const comidaRouter = require('./routes/comidaRouter')
 
-app.get("/", (req,res)=>{
-    res.json({mensaje:"aplicacion funcionando correctamente"})
-})
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+
+app.use('/API', comidaRouter)
+
+app.use((req,res) =>{
+    res.status(404).json({message: 'Ruta no encontrada'});
+});
+
+
 
 app.listen(port, ()=>{
     console.log (`Aplicacion corriendo por el puerto ${port}`)
